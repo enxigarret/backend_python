46b93bb796c7a297d8f0411cb97a80b6feea87f0
enxigarret <mclawiabroad1@hotmail.fr>
2026-02-12 17:33:51 +0100

    add pydantic and router

---
+++ fastapi_backend/app/core/config.py
@@ -0,0 +1,7 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DEBUG: bool = False
+
+
+settings = Settings()
--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI()
+from app.api.api import api_router
+from app.core.config import settings
 
+app = FastAPI(debug=settings.DEBUG,openapi_url="/api/docs.json",docs_url="/docs",redoc_url="/redoc")
 
+app.include_router(api_router,prefix="/api")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}

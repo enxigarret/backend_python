3966699db26f4bc4cddfa5644a6de7bfa28172c5
enxigarret <mclawiabroad1@hotmail.fr>
2026-02-18 16:34:15 +0100

    add pydantic file

--- fastapi_backend/app/main.py
+++ fastapi_backend/app/main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
+import sentry_sdk
 
 from app.api.api import api_router
 from app.core.config import settings
 
-app = FastAPI(debug=settings.DEBUG,openapi_url="/api/docs.json",docs_url="/docs",redoc_url="/redoc")
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    debug=settings.DEBUG,
+    openapi_url="/api/docs.json",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router,prefix="/api")
 @app.get("/")

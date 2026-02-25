ba5d77c9fc842236c0996f54e4f5cdd306fee18f
enxigarret <mclawiabroad1@hotmail.fr>
2026-02-25 12:32:08 +0100

    add token models

--- fastapi_backend/app/models.py
+++ fastapi_backend/app/models.py
@@ -31,3 +31,18 @@ class User(UserBase, table=True):
     hashed_password: str
     created_at: datetime | None = Field(default_factory=datetime.utcnow)
     
+
+    # JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=128)

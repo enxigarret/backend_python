666ab70c9b8f7f033f85100b2786e665daadf29e
enxigarret <mclawiabroad1@hotmail.fr>
2026-02-25 12:49:30 +0100

    add dependancies : data session, auth, currentuser

--- fastapi_backend/app/api/deps.py
+++ fastapi_backend/app/api/deps.py
@@ -2,9 +2,17 @@
 from sqlmodel import Session
 from typing import Annotated
 from collections.abc import Generator
+from pydantic import ValidationError
+import jwt
+from jwt.exceptions import JWTError
+
 
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
+from fastapi import Depends , HTTPException, status
+from app.models import User
+from app.core.security import ALGORITHM
+from app.models import TokenPayload
 
 
 reusable_oauth2 = OAuth2PasswordBearer(
@@ -17,5 +25,43 @@ def get_db() -> Generator[Session, None, None]:
         yield session
 
 
-SessionDep = Annotated[Session, Depends(get_db)]
+SessionDep = Annotated[Session, Depends(get_db)] 
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
+
+def get_current_user(session:SessionDep,token:TokenDep) -> User:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY, 
+            algorithms=[ALGORITHM]
+            )
+        token_data = TokenPayload.model_validate(payload)
+    except (JWTError, ValidationError):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    user = session.get(User, token_data.sub)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user"
+        )
+    return user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+def get_current_active_superuser(current_user: CurrentUser) -> User:
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The user doesn't have enough privileges",
+        )
+    return current_user
+       
 

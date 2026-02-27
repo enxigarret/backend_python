935d61067da68191802b2edfe4b18a542920f080
enxigarret <mclawiabroad1@hotmail.fr>
2026-02-27 12:22:25 +0100

    clean users router

--- fastapi_backend/app/api/api.py
+++ fastapi_backend/app/api/api.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/auth", tags=["auth"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
 
 # if settings.ENVIRONMENT == "local":

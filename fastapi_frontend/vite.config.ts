4e24789653505d84e9e5f2e20ff0b19cf5058043
enxigarret <mclawiabroad1@hotmail.fr>
2026-03-17 12:09:13 +0100

    resolve packge version conflits

--- fastapi_frontend/vite.config.ts
+++ fastapi_frontend/vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tailwindcss(),
   ],
+  
 })
